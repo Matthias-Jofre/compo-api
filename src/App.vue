6049d9361be77b0f3736eeccfdb12903fc237e53
Matthias <matthias.jofre@gmail.com>
2022-09-16 01:51:22 -0300

    update 6 files and delete 1 file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link :to="{ name: 'Counter' }">Counter</router-link> |
+    <router-link :to="{ name: 'users' }">Users</router-link>
+
   </nav>
-  <router-view/>
+
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive> 
+  </router-view>
+
 </template>
 
 <style>
---
+++ src/views/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+</template>
+
+<script>
+// import { ref } from "vue";
+import useCounter from "@/composables/useCounter";
+
+export default {
+  name: "Counter",
+  props: {},
+  emits: [],
+
+  setup() {
+    // const counter = ref(5);
+
+    // // const increase = () => {
+    // //     counter.value++
+    // // }
+
+    // // const decrease = () => {
+    // //     counter.value--
+    // // }
+
+    // return {
+    //     counter,
+
+    //     increase: () => counter.value++,
+    //     decrease: () => counter.value--
+    // }
+
+    const { counter, increase, decrease } = useCounter(0);
+
+    return {
+      counter,
+      increase,
+      decrease,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
